2e20cfcc94e42a1ad52788684f4fd243e49c182e
Adam Thrash <cathrash92@gmail.com>
2023-08-18 15:59:02 -0400

    bugfix: ignore blank lines in regions file

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,9 @@ struct Cli {
     /// this option returns individual sequences
     #[arg(short, long)]
     combine_sequences: bool,
+    /// add this suffix to the filename
+    #[arg(short, long)]
+    output: Option<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -47,79 +50,81 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut sequences: HashMap<String, Record> = HashMap::new();
 
     for raw_region in read_to_string(region_file.clone()).unwrap().lines() {
-        let mut reverse = false;
-        let mut raw_region = raw_region.to_string();
-        if raw_region.starts_with('-') {
-            reverse = true;
-            raw_region = raw_region[1..].to_string();
-        }
+        if !raw_region.is_empty() {
+            let mut reverse = false;
+            let mut raw_region = raw_region.to_string();
+            if raw_region.starts_with('-') {
+                reverse = true;
+                raw_region = raw_region[1..].to_string();
+            }
 
-        let region = raw_region.parse()?;
-        let mut record = reader.query(&region)?;
+            let region = raw_region.parse()?;
+            let mut record = reader.query(&region)?;
 
-        if reverse {
-            let definition = fasta::record::Definition::new(record.name(), None);
+            if reverse {
+                let definition = fasta::record::Definition::new(record.name(), None);
+                let start = Position::try_from(1)?;
+                let end = Position::try_from(record.sequence().len())?;
+                let mut sequence = record
+                    .sequence()
+                    .get(start..=end)
+                    .expect("could not get sequence")
+                    .to_vec();
+                sequence.reverse();
+                record = fasta::Record::new(definition, sequence.into());
+            }
+
+            let name = if args.merge_regions {
+                if let Some(split) = record.name().to_string().split_once(':') {
+                    split.0.to_string()
+                } else {
+                    record.name().to_string()
+                }
+            } else {
+                record.name().to_string()
+            };
+
+            if !ordered_sequences.contains(&name) {
+                ordered_sequences.push(name.clone());
+            }
+            let definition = fasta::record::Definition::new(name.clone(), None);
             let start = Position::try_from(1)?;
             let end = Position::try_from(record.sequence().len())?;
-            let mut sequence = record
+            let sequence = record
                 .sequence()
                 .get(start..=end)
                 .expect("could not get sequence")
                 .to_vec();
-            sequence.reverse();
             record = fasta::Record::new(definition, sequence.into());
+
+            sequences
+                .entry(name)
+                .and_modify(|existing_record| {
+                    let start = Position::try_from(1).expect("could not get position");
+                    let end = Position::try_from(existing_record.sequence().len())
+                        .expect("could not get position");
+                    let mut sequence = existing_record
+                        .sequence()
+                        .get(start..=end)
+                        .expect("could not get sequence")
+                        .to_vec();
+                    let start = Position::try_from(1).expect("could not get position");
+                    let end = Position::try_from(record.sequence().len())
+                        .expect("could not get position");
+                    let mut extended_sequence = record
+                        .sequence()
+                        .get(start..=end)
+                        .expect("could not get sequence")
+                        .to_vec();
+                    sequence.append(&mut extended_sequence);
+                    let record = fasta::Record::new(
+                        fasta::record::Definition::new(existing_record.name(), None),
+                        sequence.into(),
+                    );
+                    *existing_record = record;
+                })
+                .or_insert(record.clone());
         }
-
-        let name = if args.merge_regions {
-            if let Some(split) = record.name().to_string().split_once(':') {
-                split.0.to_string()
-            } else {
-                record.name().to_string()
-            }
-        } else {
-            record.name().to_string()
-        };
-
-        if !ordered_sequences.contains(&name) {
-            ordered_sequences.push(name.clone());
-        }
-        let definition = fasta::record::Definition::new(name.clone(), None);
-        let start = Position::try_from(1)?;
-        let end = Position::try_from(record.sequence().len())?;
-        let sequence = record
-            .sequence()
-            .get(start..=end)
-            .expect("could not get sequence")
-            .to_vec();
-        record = fasta::Record::new(definition, sequence.into());
-
-        sequences
-            .entry(name)
-            .and_modify(|existing_record| {
-                let start = Position::try_from(1).expect("could not get position");
-                let end = Position::try_from(existing_record.sequence().len())
-                    .expect("could not get position");
-                let mut sequence = existing_record
-                    .sequence()
-                    .get(start..=end)
-                    .expect("could not get sequence")
-                    .to_vec();
-                let start = Position::try_from(1).expect("could not get position");
-                let end =
-                    Position::try_from(record.sequence().len()).expect("could not get position");
-                let mut extended_sequence = record
-                    .sequence()
-                    .get(start..=end)
-                    .expect("could not get sequence")
-                    .to_vec();
-                sequence.append(&mut extended_sequence);
-                let record = fasta::Record::new(
-                    fasta::record::Definition::new(existing_record.name(), None),
-                    sequence.into(),
-                );
-                *existing_record = record;
-            })
-            .or_insert(record.clone());
     }
 
     let definition = fasta::record::Definition::new(
@@ -133,12 +138,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut scaffold = fasta::Record::new(definition, Vec::new().into());
 
     let file = File::create(format!(
-        "{}.fasta",
+        "{}{}.fasta",
         Path::new(&region_file)
             .file_stem()
             .unwrap()
             .to_str()
-            .expect("could not get str")
+            .expect("could not get str"),
+        if let Some(output) = args.output {
+            format!(".{output}")
+        } else {
+            "".to_string()
+        }
     ))?;
     let mut writer = fasta::Writer::new(file);
 
